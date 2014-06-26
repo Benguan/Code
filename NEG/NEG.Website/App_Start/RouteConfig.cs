0c98bbf7fac46fadbc3d2b11cf1f8d5f88725124
ben <ben.b.guan@newegg.com>
2014-06-26 13:54:48 +0800

    NEG commit

---
+++ NEG/NEG.Website/App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace NEG.Website
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+               name: "Default",
+               url: "{action}",
+               defaults: new { controller = "Home", action = "Tutorial", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+                name: "General",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Tutorial", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
