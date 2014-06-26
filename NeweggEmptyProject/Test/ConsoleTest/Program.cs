a25a7bc3f9bf8ff03656b3ce6b320253d5b2bcff
ben <ben.b.guan@newegg.com>
2014-06-26 09:10:37 +0800

    update

---
+++ NeweggEmptyProject/Test/ConsoleTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> intArray = new List<int>();
+            intArray.Add(1);
+            intArray.Add(2);
+            intArray.Add(3);
+            intArray.Add(4);
+            intArray.Add(5);
+
+
+            Console.WriteLine(intArray.Count);
+
+            for (var i = 0; i < 5; i++)
+            {
+                intArray.Remove(i);
+            }
+
+
+            Console.WriteLine(intArray.Count);
+
+            Console.Read();
+            
+        }
+    }
+}
